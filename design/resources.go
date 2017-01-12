5774fdd1242967c8b5d66d4b974e25bb7ed0816c
William Ondenge <ondengew@gmail.com>
2017-01-12 16:16:06 +0300

    created a withdrawals resource using actions

--- design/resources.go
+++ design/resources.go
@@ -25,3 +25,24 @@ var _ = Resource("deposits", func() {
 		Response(OK, DepositsMedia)
 	})
 })
+
+var _ = Resource("withdrawal", func() {
+	Description("Cash withdrawal by client from account at mVisa agent ")
+	BasePath("/withdrawal")
+
+	Action("create", func() {
+		Description("creates a withdrawal")
+		Routing(POST("/"))
+		Payload(WithdrawalPayload)
+		Response(Created)
+	})
+
+	Action("show", func() {
+		Description("shows a withdrawal")
+		Routing(GET("/:id"))
+		Params(func() {
+			Param("id", Integer)
+		})
+		Response(OK, WithdrawalMedia)
+	})
+})
